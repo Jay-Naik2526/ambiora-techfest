c720bcabadb44800ca8c6a9a13e092f82e363807
Jay-Naik2526 <naikjay208@gmail.com>
2026-02-12 16:16:07 +0530

    Include timeline.html in build configuration

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig({
         eventDetail: resolve(__dirname, 'event-detail.html'),
         faculty: resolve(__dirname, 'faculty.html'),
         team: resolve(__dirname, 'team_members.html'),
-        admin: resolve(__dirname, 'admin.html')
+        admin: resolve(__dirname, 'admin.html'),
+        timeline: resolve(__dirname, 'timeline.html')
       }
     }
   },
